9eee12f2460817f940acdac8bddcff0ede22b94d
Jaretz <bmcx12@gmail.com>
2016-07-18 21:13:48 +0200

    Initial structure apps.

---
+++ teckels/main/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+
+from main.models import *
+
+admin.site.register(Category)
+admin.site.register(Item)
+admin.site.register(Shop)
+admin.site.register(Banner)
---
+++ teckels/main/models/__init__.py
@@ -0,0 +1,4 @@
+from main.models.category import Category
+from main.models.item import Item
+from main.models.shop import Shop
+from main.models.banner import Banner
