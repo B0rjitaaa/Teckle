d45890ef9246bceae753e5a1c018e458ad9fd161
Jaretz <bmcx12@gmail.com>
2016-07-18 21:27:58 +0200

    Adapted urls new format.

--- teckels/teckels/settings.py
+++ teckels/teckels/settings.py
@@ -73,7 +73,7 @@ ROOT_URLCONF = 'teckels.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['main', 'dashboard'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
